3a53c0084a5b88a2370ea718080a7eeb95adda2f
wyc <810623295@qq.com>
2020-08-08 21:36:27 +0800

    md

--- src/utils/dataShort.js
+++ src/utils/dataShort.js
@@ -1,12 +1,21 @@
 const cardList = [
     {
         imgurl:'https://ainyi.com/krryblog/upload/2019/05/09/cd05bcc9-367e-4195-8b0e-84f6ede337b8.jpg',
+        title:'Hi everyone',
+        range:'置顶,introduce',
+        url:'2020/introduce',
+        fileName:'test.md',
+        desc:'em...算是简单介绍几句吧，关于网站什么的，毕竟第一次搞这种东西哈哈',
+        date:'2020-08-08',
+    },
+    {
+        imgurl:'https://ainyi.com/krryblog/upload/2019/05/09/cd05bcc9-367e-4195-8b0e-84f6ede337b8.jpg',
         title:'this is title 这是测试标题',
         range:'js,Vue',
         url:'2020/0101',
         fileName:'test.md',
         desc:'测试的desc',
-        date:'2020-0101',
+        date:'2020-01-01',
     },
     {
         imgurl:'/static/img/sea.jpg',
@@ -15,7 +24,7 @@ const cardList = [
         url:'2020/0201',
         fileName:'test.md',
         desc:'测试的desc',
-        date:'2020-0101'
+        date:'2020-01-01'
     },
     {
         imgurl:null,
@@ -24,7 +33,7 @@ const cardList = [
         url:'2020/0201',
         fileName:'test.md',
         desc:'测试的desc',
-        date:'2020-0101'
+        date:'2020-01-01'
     },
 ]
 
